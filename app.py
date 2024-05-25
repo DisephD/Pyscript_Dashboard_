021cec6ad64a31cb5ae769cf24d64743ec079700
DisephD <153353578+DisephD@users.noreply.github.com>
2024-05-25 11:17:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,63 +1,64 @@
-import js
+from js import Plotly, JSON, Uint8Array, File, URL
 import pandas as pd
-import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 from pyodide.http import open_url
-from pyscript import display
+from pyscript import display, document
 import warnings
 warnings.filterwarnings("ignore")
 
-url = "https://raw.githubusercontent.com/DisephD/pyscript_tutorial/main/drink_water_data.csv"
+url = "https://raw.githubusercontent.com/DisephD/Pyscript-Dashboad/main/data/water_data_processed%20(1).csv"
 
 data = pd.read_csv(open_url(url), parse_dates=["Date"])
-water_data = data.query("year == 2024")
+water_data = data
 
+table_daily_total = None
 
-# Format a number to a string eg 8 to 'week 8'
 def format_week_num(week_num): 
   return f"Week {week_num}"
 
-
 #create a html representation of a plotly graph
 def plotly_to_json (fig, target_id): 
-    graphJSON = fig.to_json()
-    js.Plotly.newPlot (target_id, js.JSON.parse(graphJSON))
+
+    chart_json = fig.to_json()
+    Plotly.newPlot (target_id, JSON.parse(chart_json))
 
 
-def display_str(variable, target):
-    js.document.getElementById(target).innerHTML =''
+def display_stats(variable, target):
+
+    document.getElementById(target).innerHTML =''
     var = (f"{variable} ml")
     display (var, target = target)
 
 
 #create statistics and parse to html
-def stats(filtered_df):
-    daily_total = filtered_df.groupby("day")["Water(ml)"].sum().reset_index()
+def monthly_stats(filtered_df):
 
+    daily_total = filtered_df.groupby("Day")["Water(ml)"].sum().reset_index()
     total_water = int(daily_total["Water(ml)"].sum())
     avg_water = int(round((total_water/len(daily_total)),1))
     highest_water = int(daily_total["Water(ml)"].max())
 
-    display_str(total_water, "total_water")
-    display_str(avg_water, "avg_water")
-    display_str(highest_water, "highest_water")
+    display_stats(total_water, "total_water")
+    display_stats(avg_water, "avg_water")
+    display_stats(highest_water, "highest_water")
 
 
 #create total statistics for all the months 
 def total_month_stats():
-    daily_total = water_data.groupby("day")["Water(ml)"].sum().reset_index()
 
+    daily_total = water_data.groupby("Day")["Water(ml)"].sum().reset_index()
     all_total = int(daily_total["Water(ml)"].sum())
     all_avg = int(round((all_total/len(daily_total)),1))
     all_highest = int(daily_total["Water(ml)"].max())
 
-    display_str(all_total, target= "all_total")
-    display_str(all_avg, target = "all_avg")
-    display_str(all_highest, target = "all_highest")
+    display_stats(all_total, target= "all_total")
+    display_stats(all_avg, target = "all_avg")
+    display_stats(all_highest, target = "all_highest")
 
 
 def circle_chart (filtered_df):
+
     total_water = int(filtered_df["Water(ml)"].sum())
     target= 62000
 
@@ -70,16 +71,15 @@ def circle_chart (filtered_df):
     ))
 
     fig.update_layout(width=300, height=300, showlegend=False, margin=dict(t=10,l=10,b=5,r=10),)
-
     plotly_to_json(fig, "circle_chart")
     
 
-
 #create and format a line chart with plotly express 
 def line_chart (filtered_df):
-    daily_total = filtered_df.groupby("day")["Water(ml)"].sum().reset_index()
+
+    daily_total = filtered_df.groupby("Day")["Water(ml)"].sum().reset_index()
     
-    fig = px.line(daily_total, x="day", y="Water(ml)", height=270, width= 670, markers=True)
+    fig = px.line(daily_total, x="Day", y="Water(ml)", height=270, width= 670, markers=True)
     fig.update_traces (marker_size = 6, texttemplate='%{text:.2s}', line_color= "#4587D9")
 
     fig.update_layout(plot_bgcolor="rgb(254,254,254)", margin=dict(t=50,l=10,b=5,r=10), 
@@ -88,35 +88,40 @@ def line_chart (filtered_df):
                       yaxis=dict( title=None, titlefont_size=14, tickfont_size=11, tickfont_color= "#A9A9AB", ),
                       xaxis = dict(title=None, tickfont_size =11,  tickfont_color= "#A9A9AB"),
                       )
+    
     fig.add_hline(y=2000, line_width=1,  line_dash="dash", line_color="#bbd9fe")
     plotly_to_json (fig, "line_chart")
 
 
+
 #create and format a bar chart with plotly express 
 def bar_chart (filtered_df): 
-    bar_daily_total = filtered_df.groupby("week")["Water(ml)"].sum().reset_index()
 
-    fig = px.bar (bar_daily_total, x ="week", y = "Water(ml)", title= "", labels ="", 
+    bar_daily_total = filtered_df.groupby("Week")["Water(ml)"].sum().reset_index()
+
+    fig = px.bar (bar_daily_total, x ="Week", y = "Water(ml)", title= "", labels ="", 
                   color_discrete_sequence=["#3C91E6"])
-    fig.update_xaxes(tickvals= bar_daily_total["week"].unique(),
-                     ticktext=[format_week_num(week)for week in bar_daily_total['week'].unique()])
+    fig.update_xaxes(tickvals= bar_daily_total["Week"].unique(),
+                     ticktext=[format_week_num(week)for week in bar_daily_total['Week'].unique()])
     fig.update_layout(
         plot_bgcolor="rgb(254,254,254,0.7)", margin=dict(t=20,l=10,b=0,r=10), width=300, height = 300,
         xaxis_tickfont_size=14,yaxis=dict( title='', titlefont_size=14, tickfont_size=13),
         xaxis = dict(title="", tickfont_size =13), bargap=0.3, 
     )
     fig.update_traces(width=0.5)
-    fig.add_hline(y=14000, line_width=3,  line_dash="dash", line_color="#bbd9fe")
+    fig.add_hline(y=14000, line_width=2,  line_dash="dash", line_color="#E92022")
 
     plotly_to_json (fig, "bar_chart")
 
 
 #create and format a table with plotly graph objects
 def table_chart (filtered_df):
-    table_daily_total = filtered_df.groupby("Date")["Water(ml)"].sum().reset_index()
-    table_daily_total["Daily_Change"] = np.round(table_daily_total["Water(ml)"].pct_change()*100,1).fillna(0).apply(lambda x: f"+{x}" if x > 0 else str(x)) + "%"
-    table_daily_total["Daily_Change"] = table_daily_total["Daily_Change"].astype("category")
 
+    global table_daily_total
+    table_daily_total = filtered_df.groupby("Date")["Water(ml)"].sum().reset_index()
+    table_daily_total["Daily_Change"] = table_daily_total["Water(ml)"].pct_change().mul(100).round(1).fillna(0).apply(lambda x: f"+{x}" if x > 0 else str(x)) + "%"
+    table_daily_total["Daily_Change"] = table_daily_total["Daily_Change"].astype("category")
+    
     font_color = ["#333131", "#333131",
                   ["#8DE28D" if "+" in x else "#FE5555" if "-" in x else "#333131" for x in list(table_daily_total["Daily_Change"])]]
 
@@ -136,40 +141,35 @@ def table_chart (filtered_df):
     fig.update_layout(plot_bgcolor="rgb(254,254,254)",height= 270, margin=dict(t=5,l=10,b=10,r=10),)
     plotly_to_json (fig, "table")
 
+    return table_daily_total
+
 
 #create and format a heatmap with plotly express
 def heat_chart (filtered_df): 
-    filtered_df['time'] = pd.to_datetime(filtered_df['hour'], format='%H: %M')
-    time_bins = [0, 11, 18, 21, 24]
-    time_labels = ['Morning', 'Noon', 'Evening', 'Night']
-    filtered_df['time_of_day'] = pd.cut(filtered_df['time'].dt.hour, bins=time_bins, labels=time_labels, right=False)
-    filtered_df.drop("time", axis=1, inplace=True)
 
-    heatmap_data = filtered_df.groupby(['day_of_the_week', 'time_of_day'])["Water(ml)"].sum().reset_index()
+    heatmap_data = filtered_df.groupby(['Weekday_name', 'Time_of_day'])["Water(ml)"].sum().reset_index()
     custom_order = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat','Sun', ]
-    heatmap_data['day_of_the_week'] = pd.Categorical(heatmap_data['day_of_the_week'], categories=custom_order, ordered=True)
-    heatmap_data = heatmap_data.sort_values(by='day_of_the_week')
+    heatmap_data['Weekday_name'] = pd.Categorical(heatmap_data['Weekday_name'], categories=custom_order, ordered=True)
+    day_order = ["Morning", "Noon", "Evening", "Night"]
+    heatmap_data['Time_of_day'] = pd.Categorical(heatmap_data['Time_of_day'], categories=day_order, ordered=True)
 
-    heatmap_pivot= heatmap_data.pivot_table(index="time_of_day", columns= "day_of_the_week", values= "Water(ml)", aggfunc="sum", fill_value= 0)
+    heatmap_pivot= heatmap_data.pivot_table(index="Time_of_day", columns= "Weekday_name", values= "Water(ml)", aggfunc="sum", fill_value= 0)
 
     fig= px.imshow(heatmap_pivot, color_continuous_scale='Blues', height=300, width = 240)
     fig.update_traces(showscale=False, coloraxis=None,colorscale= "Blues")
-
     fig.update_layout(plot_bgcolor="rgb(254,254,254)", title='',margin=dict(t=10,l=0,b=80,r=20), 
         xaxis_tickfont_size=14,
         yaxis=dict( title=None, titlefont_size=14, tickfont_size=13),
         xaxis = dict(title=None, tickfont_size =13, side="top", tickangle=90),
     )
-    fig.update_xaxes(side="top", tickangle=90)
     plotly_to_json (fig, "heatmap")
 
 
-
 #return all the different chart functions
-def plot(selected_month):  
-   filtered_df = water_data[water_data["month"]==selected_month]
+def plot_charts(selected_month):  
+   filtered_df = water_data[water_data["Month"]==selected_month]
    all_stats = total_month_stats()
-   kpis = stats(filtered_df)
+   kpis = monthly_stats(filtered_df)
    line = line_chart(filtered_df)
    table = table_chart(filtered_df)
    circle = circle_chart(filtered_df)
@@ -179,9 +179,24 @@ def plot(selected_month):
 
 
 #create a dropdown event catcher
-def dropdown_event():
-    selected_month = js.document.getElementById("selected_month").value
-    plot(selected_month)
+def dropdown_event(event):
+    selected_month = document.getElementById("dropdown_menu").value
+    plot_charts(selected_month)
 
 
-plot("January")
+def download_file(*args):
+    filename = "waterdata.csv"
+    table_daily_total.to_csv(filename, index=False)
+
+    with open (filename, "rb") as data:
+        my_stream = data.read()
+        js_array = Uint8Array.new(len(my_stream))
+        js_array.assign(my_stream)
+        file = File.new ([js_array], "unused.csv", {type: "text/csv"})
+        url = URL.createObjectURL(file)
+        hidden_link = document.createElement("a")
+        hidden_link.setAttribute("download", "waterdata.csv")
+        hidden_link.setAttribute("href", url)
+        hidden_link.click()
+
+plot_charts("January")
